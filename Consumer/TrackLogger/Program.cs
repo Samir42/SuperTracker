ac558c8b924ab68857882632c92c27ce33a749da
Samir42 <samir.netdev@outlook.com>
2023-12-10 16:10:24 +0400

    Prepare the consumer  - still needs refactoring

---
+++ Consumer/TrackLogger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Task.Delay(10000).Wait();
+
+
+var factory = new ConnectionFactory { HostName = "rabbitmq", Port=5672, UserName="guest", Password="guest" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "SuperTracker",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "SuperTracker",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
